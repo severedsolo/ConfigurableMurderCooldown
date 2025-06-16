cb15185734e0d47de469479477d6a9bfabed5534
severedsolo <severedsolo@users.noreply.github.com>
2025-06-16 22:54:31 +0100

    Update PatchPickNewVictim.cs

--- ConfigurableMurderCooldown/PatchPickNewVictim.cs
+++ ConfigurableMurderCooldown/PatchPickNewVictim.cs
@@ -9,6 +9,7 @@ public class PatchPickNewVictim
     internal static void Postfix()
     {
         CooldownController.SetCooldown(false);
+        //Reset the current cooldown to 0 so we don't pause twice.
         MurderController.Instance.pauseBetweenMurders = 0;
         Plugin.ModLog("New Victim. Resetting cooldown");
             
